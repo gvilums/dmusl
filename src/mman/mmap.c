ea2405b0b068f0c21e13f84a6f6d2af166b3989f
Georgijs Vilums <georgijs.vilums@gmail.com>
2023-02-23 10:36:16 +0100

    replace mmap with simple bump allocator

--- src/mman/mmap.c
+++ src/mman/mmap.c
@@ -11,7 +11,7 @@ weak_alias(dummy, __vm_wait);
 #define UNIT SYSCALL_MMAP2_UNIT
 #define OFF_MASK ((-0x2000ULL << (8*sizeof(syscall_arg_t)-1)) | (UNIT-1))
 
-void *__mmap(void *start, size_t len, int prot, int flags, int fd, off_t off)
+void *__mmap_real(void *start, size_t len, int prot, int flags, int fd, off_t off)
 {
 	long ret;
 	if (off & OFF_MASK) {
@@ -34,6 +34,33 @@ void *__mmap(void *start, size_t len, int prot, int flags, int fd, off_t off)
 	if (ret == -EPERM && !start && (flags&MAP_ANON) && !(flags&MAP_FIXED))
 		ret = -ENOMEM;
 	return (void *)__syscall_ret(ret);
+}
+
+uintptr_t __mmap_base_addr = 0;
+uintptr_t __mmap_end_addr = 0;
+
+void *__mmap(void *start, size_t len, int prot, int flags, int fd, off_t off) {
+	if (__mmap_base_addr == 0) {
+		const size_t map_size = 1ull << 30;
+		__mmap_base_addr = (uintptr_t)__mmap_real(NULL, map_size, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+		__mmap_end_addr = __mmap_base_addr + map_size;
+	}
+
+	size_t aligned_len = ((len - 1) | (PAGE_SIZE - 1)) + 1;
+	if (fd != -1) {
+		errno = EBADF;
+		return MAP_FAILED;
+	}
+	if (__mmap_base_addr + aligned_len < __mmap_base_addr || off != 0 || (prot & PROT_EXEC)) {
+		errno = EINVAL;
+		return MAP_FAILED;
+	}
+	if (__mmap_base_addr + aligned_len > __mmap_end_addr) {
+		errno = ENOMEM;
+		return MAP_FAILED;
+	}
+	__mmap_base_addr += aligned_len;
+	return (void *)(__mmap_base_addr - aligned_len);
 }
 
 weak_alias(__mmap, mmap);
--- src/mman/munmap.c
+++ src/mman/munmap.c
@@ -4,10 +4,14 @@
 static void dummy(void) { }
 weak_alias(dummy, __vm_wait);
 
-int __munmap(void *start, size_t len)
+int __munmap_real(void *start, size_t len)
 {
 	__vm_wait();
 	return syscall(SYS_munmap, start, len);
+}
+
+int __munmap(void *start, size_t len) {
+	return 0;
 }
 
 weak_alias(__munmap, munmap);
