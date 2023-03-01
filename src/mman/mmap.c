0aed014504bdc94cd6e97544efdf995781546991
Georgijs Vilums <georgijs.vilums@gmail.com>
2023-03-01 15:17:20 +0100

    change mmap to use _end symbol for now

--- src/mman/mmap.c
+++ src/mman/mmap.c
@@ -36,13 +36,16 @@ void *__mmap_real(void *start, size_t len, int prot, int flags, int fd, off_t of
 	return (void *)__syscall_ret(ret);
 }
 
-uintptr_t __mmap_base_addr = 0;
-uintptr_t __mmap_end_addr = 0;
+#define PAGE_ALIGN(x) (((x - 1) | (PAGE_SIZE - 1)) + 1)
 
 void *__mmap(void *start, size_t len, int prot, int flags, int fd, off_t off) {
+	extern char _end;
+	static uintptr_t __mmap_base_addr = 0;
+	static uintptr_t __mmap_end_addr = 0;
+
 	if (__mmap_base_addr == 0) {
-		const size_t map_size = 1ull << 30;
-		__mmap_base_addr = (uintptr_t)__mmap_real(NULL, map_size, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+		const size_t map_size = 1ull << 30; // 1GiB heap for now
+		__mmap_base_addr = PAGE_ALIGN((uintptr_t)&_end);
 		__mmap_end_addr = __mmap_base_addr + map_size;
 	}
 
