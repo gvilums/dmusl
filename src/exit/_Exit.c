6df7ef7ffc39e9d516ac18c8481bdc004f903d24
Georgijs Vilums <georgijs.vilums@gmail.com>
2023-03-02 17:23:18 +0100

    update exit

--- src/exit/_Exit.c
+++ src/exit/_Exit.c
@@ -1,8 +1,29 @@
+#include <stddef.h>
 #include <stdlib.h>
 #include "syscall.h"
 
+typedef struct{
+  size_t size;
+  void* address;
+} ioStruct;
+
+static ioStruct* const inputRoot;
+static ioStruct* outputRoot;
+static const unsigned int inputNumber;
+static unsigned int outputNumber = 0;
+static const unsigned int maxOutputNumber;
+
+static const void * __capability const returnPair;
+
+
 _Noreturn void _Exit(int ec)
 {
-	__syscall(SYS_exit_group, ec);
-	for (;;) __syscall(SYS_exit, ec);
+	// __syscall(SYS_exit_group, ec);
+	// for (;;) __syscall(SYS_exit, ec);
+	__asm__ volatile(
+    // "ldr c0, %w[] \n"
+		"ldpbr c29, [%w[returnPair]] \n"
+		: : [returnPair] "r" (returnPair) : "c0", "c29"
+	);
+	__builtin_unreachable();
 }
